0b0dcf5273d42f83301ea36d2fd98e3c047d6aec
Ivan Voras <ivoras@gmail.com>
2017-04-29 03:29:58 +0200

    The genesis block

--- src/blockchain.go
+++ src/blockchain.go
@@ -6,6 +6,9 @@ import (
 	"log"
 )
 
+// GenesisBlockHash is the SHA256 hash of the genesis block payload
+const GenesisBlockHash = "85404bd215ff98f3ea04b8c0c86180a79f2e1c38740d03b02f0bfff358dd1347"
+
 func blockchainInit() {
 	if dbGetBlockchainHeight() == -1 {
 		log.Println("Noticing the existence of the Genesis block. Let there be light.")
