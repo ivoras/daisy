8565c1dfacdd653c69be7bd61ddad8e3aee6a67f
Ivan Voras <ivoras@gmail.com>
2019-02-02 00:50:21 +0100

    Better message

--- crypto.go
+++ crypto.go
@@ -23,7 +23,7 @@ type ecdsaSignature struct {
 
 func cryptoInit() {
 	if dbNumPrivateKeys() == 0 {
-		log.Println("Generating the keypair...")
+		log.Println("Generating the initial wallet keypair...")
 		generatePrivateKey(-1)
 		log.Println("Generated.")
 	}
